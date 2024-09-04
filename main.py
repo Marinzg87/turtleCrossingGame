6f9a64f7b0497963b5046668da4ea884636ff8fd
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-09-04 14:47:23 +0200

    Implement CarManager class. Implement basic logic loop.

--- main.py
+++ main.py
@@ -9,13 +9,30 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+cars = []
+car = CarManager()
+cars.append(car)
 
 screen.listen()
 screen.onkey(player.move, "Up")
+
+loop_counter = 0
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    loop_counter += 1
+    if loop_counter == 6:
+        new_car = CarManager()
+        cars.append(new_car)
+        loop_counter = 0
+    for car in cars:
+        car.move()
+
+    # detect collision with car
+    for car in cars:
+        if car.distance(player) < 20:
+            game_is_on = False
 
 screen.exitonclick()
